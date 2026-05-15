d69c929c8856bf05c9257fdc98c05eb67679917a
udcyber <udewaleyne.pro@gmail.com>
2026-05-15 14:15:13 +0200

    Pascal's Triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -12,7 +12,7 @@ def pascal_triangle(n):
     row = []
     prev_row = []
     for i in range(0, n + 1):
-        row = [x > 0 and x < i - 1 and i > 2 and prev_row[x - 1]
+        row = [x > 0 and x < i - 1 and i > 2 and prev_row[x-1]
                + prev_row[x] or 1 for x in range(0, i)]
         prev_row = row
         pascal_triangle += [row]
